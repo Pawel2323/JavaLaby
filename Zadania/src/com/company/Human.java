edd09036e555e8730045df9d5bdef61575458274
Pawel2323 <83045162+Pawel2323@users.noreply.github.com>
2021-06-03 21:44:10 +0200

    Zadanie 0

---
+++ Zadania/src/com/company/Human.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Human {
+
+        String fName;
+        String lName;
+        String Phone;
+
+}
---
+++ Zadania/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Human me = new Human();
+        me.fName = "Pawel";
+        me.lName = "Pietrolaj";
+
+
+        Phone OnePlus = new Phone();
+        OnePlus.screenSize = 6.3;
+        OnePlus.producer = "OnePlus";
+        OnePlus.model = "6";
+        OnePlus.System = "Android11";
+
+
+    }
+}
