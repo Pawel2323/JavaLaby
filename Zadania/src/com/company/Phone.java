1b4b843797f76a0ab0148b85bb664ec40e94983c
Pawel2323 <83045162+Pawel2323@users.noreply.github.com>
2021-06-03 22:30:05 +0200

    Add files via upload

--- Zadania/src/com/company/Phone.java
+++ Zadania/src/com/company/Phone.java
@@ -3,6 +3,17 @@ package com.company;
 public class Phone {
     String producer;
     String model;
-    Double screenSize;
+    String screenSize;
     String System;
+
+    public Phone(String producer, String model, String screenSize, String System){
+        this.producer = producer;
+        this.model = model;
+        this.screenSize = screenSize;
+        this.System = System;
+    }
+
+
+
+
 }
